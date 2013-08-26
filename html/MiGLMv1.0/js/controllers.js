1202933e051f5b5681c752a19be63f5a50d89ff7
Tito Agudelo <titoagudelo@Administradors-MacBook-Pro.local>
2013-08-26 12:17:47 -0500

    login angular ng-show

--- html/MiGLMv1.0/js/controllers.js
+++ html/MiGLMv1.0/js/controllers.js
@@ -7,6 +7,9 @@ function CtrlForm($scope) {
     $scope.glm = $scope.email.endsWith('@glm.edu.co');
     $scope.google = $scope.email.endsWith('@gmail.com');
     $scope.outlook = $scope.email.endsWith('@hotmail.com');
-    $scope.yahoo = $scope.email.endsWith('@yahoo');
+    $scope.yahoo = $scope.email.endsWith('@yahoo.com');
+    $scope.user = $scope.email.endsWith('@register.com');
+    $scope.newpassword = $scope.email.endsWith('@password.com');
+    $scope.newregister = $scope.email.endsWith('@noregister.com');
   }
 }
